68a030f5c127440cfd888ef1b793993156e328ef
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-08-26 13:00:19 +0200

    add orders fetch

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState, useContext } from "react";
+import TokenContext from "../Contexts/TokenContext";
+
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+  const { token } = useContext(TokenContext);
+
+  useEffect(function () {
+    fetch("http://localhost:3001/orders", {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
+
+  console.log("orders", orders);
+  console.log("token", token);
+  console.log("orderedProducts", orders.products);
+
   return (
     <div>
-      <h1>Davs</h1>
+      <h1>Bestillinger</h1>
+      <div>
+        <ul>
+          {orders.map((order) => (
+            <li>{order.customerId}</li>
+          ))}
+        </ul>
+        <ul>
+          {orders.products.map((orderedProducts) => (
+            <li>{orderedProducts.amount}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
