6cff276701c69594d6a3481e27659ee74e0b4021
Maryam <mahta.ir@gmail.com>
2022-09-01 12:53:58 +0200

    new layout

--- src/Pages/Orders.jsx
+++
@@ -1,9 +0,0 @@
-const Orders = () => {
-  return (
-    <div>
-      <h1>Davs</h1>
-    </div>
-  );
-};
-
-export default Orders;
--- src/Pages/ProductCatalogue.jsx
+++
@@ -1,9 +0,0 @@
-const ProductCatalogue = () => {
-  return (
-    <div>
-      <h1>Varekatalog</h1>
-    </div>
-  );
-};
-
-export default ProductCatalogue;
